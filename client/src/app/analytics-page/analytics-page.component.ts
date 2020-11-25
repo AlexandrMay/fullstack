8f6660f9a0b970eb756aca549fc1a9a398755863
AlexandrMay <maysalexandr@gmail.com>
2020-11-25 15:40:51 +0200

    analytics adding

--- client/src/app/analytics-page/analytics-page.component.ts
+++ client/src/app/analytics-page/analytics-page.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AnalyticsPage } from './../shared/interfaces';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { AnalyticsService } from '../shared/services/analytics.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-analytics-page',
   templateUrl: './analytics-page.component.html',
   styleUrls: ['./analytics-page.component.css']
 })
-export class AnalyticsPageComponent implements OnInit {
+export class AnalyticsPageComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('gain') gainRef: ElementRef;
+  @ViewChild('order') orderRef: ElementRef;
+
+  average: number;
+  pending = true;
+  aSub: Subscription;
+
+  constructor(private service: AnalyticsService) { }
+
+  ngAfterViewInit(): void {
+    const gainConfig: any = {
+      label: 'Выручка',
+      color: 'rgb(255, 99, 132)'
+    };
+    const orderConfig: any = {
+      label: 'Заказы',
+      color: 'rgb(54, 162, 235)'
+    };
+    this.aSub = this.service.getAnalytics().subscribe((data: AnalyticsPage) => {
+      this.average = data.average;
+      gainConfig.labels = data.chart.map(item => item.label);
+      gainConfig.data = data.chart.map(item => item.gain);
+
+      orderConfig.labels = data.chart.map(item => item.label);
+      orderConfig.data = data.chart.map(item => item.order);
+
+      const gainCtx = this.gainRef.nativeElement.getContext('2d');
+      gainCtx.canvas.height = '300px';
+
+      const orderCtx = this.orderRef.nativeElement.getContext('2d');
+      gainCtx.canvas.height = '300px';
+
+      // tslint:disable-next-line:no-unused-expression
+      new Chart(gainCtx, createChartConfig(gainConfig));
+      // tslint:disable-next-line:no-unused-expression
+      new Chart(orderCtx, createChartConfig(orderConfig));
+
+      this.pending = false;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe();
+    }
+  }
+}
+
+function createChartConfig({labels, data, label, color}) {
+  return {
+    type: 'line',
+    options: {
+      responsive: true
+    },
+    data: {
+      labels,
+      datasets: [
+        {
+          label,
+          data,
+          borderColor: color,
+          steppedLine: false,
+          fill: false
+        }
+      ]
+    }
+  };
 }
--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { IMaterialInstance, MaterialService } from './../shared/classes/material.service';
+import { OverviewPage } from './../shared/interfaces';
+import { Observable } from 'rxjs';
+import { AnalyticsService } from './../shared/services/analytics.service';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
-export class OverviewPageComponent implements OnInit {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tapTarget') tapTargetRef: ElementRef;
+  tapTarget: IMaterialInstance;
+  data$: Observable<OverviewPage>;
+  yesterday = new Date();
+
+  constructor(private service: AnalyticsService) { }
+
+  ngOnDestroy(): void {
+    this.tapTarget.destroy();
+  }
+  ngAfterViewInit(): void {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef);
+  }
 
   ngOnInit(): void {
+    this.data$ = this.service.getOverview();
+    this.yesterday.setDate(this.yesterday.getDate() - 1);
+  }
+
+  openInfo() {
+    this.tapTarget.open();
   }
 
 }
